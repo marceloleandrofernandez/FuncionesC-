128e1499099300d6dce3fd77aba0b3b22f78c2ff
marceloleandrofernandez <marceloleandrofernandez@gmail.com>
2017-11-02 09:57:40 -0300

    Create punt_estruct.cpp

---
+++ Ejercicios/promoc/punt_estruct.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+main()
+{
+struct {
+   char initial;
+   int age;
+   int grade;
+   } kids[12],*point;
+
+int index;
+
+   for (index = 0;index < 12;index++) {
+      point = kids + index;
+      point->initial = 'A' + index;
+      point->age = index ;
+      point->grade = 84;
+   }
+
+   kids[3].age = kids[5].age = 17;
+   kids[2].grade = kids[6].grade = 92;
+   kids[4].grade = 57;
+
+   for (index = 0;index < 12;index++) {
+      point = kids + index;
+      printf("%c tiene %d años y un grado escolar de %d\n",
+             (*point).initial, kids[index].age,
+             point->grade);
+   }
+}
